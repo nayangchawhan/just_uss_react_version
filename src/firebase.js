e3fb404d4ac9029cf33dacb63ba94766af1fb7c6
nayangchawhan <nayangc2005@gmail.com>
2025-06-25 12:43:39 +0530

    Refactor Firebase configuration: replace hardcoded values with environment variables for improved security

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCbNLP1tNrUVZFeKtdCV_jWSTC3i2GUUkQ",
-    authDomain: "just-us-8c275.firebaseapp.com",
-    projectId: "just-us-8c275",
-    storageBucket: "just-us-8c275.appspot.com",
-    messagingSenderId: "109163002325",
-    appId: "1:109163002325:web:fd7119118257fdf234655c",
-    measurementId: "G-B7K091Q0ZL"
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_FIREBASE_APP_ID,
+        measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
